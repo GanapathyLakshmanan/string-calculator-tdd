68e996da6329ce225df1d0fbc7254ec77637c88c
Ganapathy Lakshmanan <ganapathyror@gmail.com>
2025-01-26 18:42:40 +0530

    Handled single and two comma-separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,14 @@
 class StringCalculator
-def add(numbers)
-return 0 if numbers.empty?
+	def add(numbers)
+		return 0 if numbers.empty?
+		return numbers.to_i if !numbers.include?(",")
+    
+		numbers.split(",").map(&:to_i).sum
+	end
 end
-end
+
+# Testing
+calc = StringCalculator.new
+puts calc.add("")     # Output: 0
+puts calc.add("1")    # Output: 1
+puts calc.add("1,2")  # Output: 3
