54de2142d2b8b39ce271f002345da39a6598bda4
Ganapathy Lakshmanan <ganapathyror@gmail.com>
2025-01-26 19:05:30 +0530

    Handled Support for different delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,10 +1,16 @@
 class StringCalculator
 	def add(numbers)
 		return 0 if numbers.empty?
-		return numbers.to_i if !numbers.include?(",")
+		
+		delimiter = ","
+		if numbers.start_with?("//")
+			parts = numbers.split("\n", 2)
+			delimiter = parts[0][2]
+			numbers = parts[1]
+		end
     
-	    numbers = numbers.gsub("\n", ",")
-		numbers.split(",").map(&:to_i).sum
+	    numbers = numbers.gsub("\n", delimiter)
+		numbers.split(delimiter).map(&:to_i).sum
 	end
 end
 
@@ -15,3 +21,4 @@ puts calc.add("1")    # Output: 1
 puts calc.add("1,2")  # Output: 3
 puts calc.add("1,2,3,4")   # Output: 10
 puts calc.add("1\n2,3")    # Output: 6
+puts calc.add("//;\n1;2")  # Output: 3
