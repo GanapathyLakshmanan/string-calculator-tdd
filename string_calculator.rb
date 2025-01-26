826571c774f3b15d58bb92c3a7fac7af85f9d5bd
Ganapathy Lakshmanan <ganapathyror@gmail.com>
2025-01-26 18:54:13 +0530

    Handled Newlines as Delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,7 @@ class StringCalculator
 		return 0 if numbers.empty?
 		return numbers.to_i if !numbers.include?(",")
     
+	    numbers = numbers.gsub("\n", ",")
 		numbers.split(",").map(&:to_i).sum
 	end
 end
@@ -13,3 +14,4 @@ puts calc.add("")     # Output: 0
 puts calc.add("1")    # Output: 1
 puts calc.add("1,2")  # Output: 3
 puts calc.add("1,2,3,4")   # Output: 10
+puts calc.add("1\n2,3")    # Output: 6
